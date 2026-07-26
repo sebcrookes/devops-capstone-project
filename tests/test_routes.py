51bf08deef91912a88275630a47959c41a4ef55e
Sebastian Crookes <71226595+sebcrookes@users.noreply.github.com>
2026-07-26 18:43:40 +0100

    Updated tests for flake8

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -55,11 +55,9 @@ class TestAccountService(TestCase):
         """Runs once after each test case"""
         db.session.remove()
 
-
     ######################################################################
     #  H E L P E R   M E T H O D S
     ######################################################################
-
 
     def _create_accounts(self, count):
         """Factory method to create accounts in bulk"""
@@ -77,11 +75,9 @@ class TestAccountService(TestCase):
             accounts.append(account)
         return accounts
 
-
     ######################################################################
     #  A C C O U N T   T E S T   C A S E S
     ######################################################################
-
 
     def test_index(self):
         """It should get 200_OK from the Home Page"""
