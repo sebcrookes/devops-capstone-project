e764129028783ff01830680befb3f4587f40bc5f
Sebastian Crookes <71226595+sebcrookes@users.noreply.github.com>
2026-07-26 13:28:02 +0100

    Wrote test for listing all accounts

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,15 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+
+    def test_list_accounts(self):
+        """Test to ensure that we can fetch a correct list of all accounts"""
+
+        self._create_accounts(5)
+
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.get_json()
+
+        self.assertEqual(len(data), 5)
